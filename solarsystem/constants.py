5e54a9c139d7a8c94b792b518b07cee1c10baf91
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2021-12-10 12:18:55 +0100

    updated constants

--- solarsystem/constants.py
+++ solarsystem/constants.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 # Fundamental constants
-G = 6.67e-11        # m^3 kg^-1 s^-2
-Msun = 1.988e30     # kg
-Mearth = 5.9722e24  # kg
+G = 6.67430e-11     # m^3 kg^-1 s^-2
+Msun = 1.98847e30   # kg
+Mearth = 5.97219e24 # kg
 Mjup = 1.8981e27    # kg
 AU = 149597870700.  # m
 day = 86400         # s
@@ -12,7 +12,7 @@ c = 299792458.      # m s^-2
 # Useful quantities
 c2 = c*c
 arcsec = 2*np.pi/(360*3600)
-mercury_precession_GR = 42.9799*arcsec # arcsec/century
+mercury_precession_GR = 42.9799*arcsec/100. # arcsec/century
 mercury_eccentricity = 0.20563069
 
 # Solar system masses [kg] - from https://solarsystem.nasa.gov/planet-compare/
@@ -20,7 +20,7 @@ masses = {
     'sun'     : Msun,
     'earth'   : Mearth,
     'moon'    : 7.3476e22,
-    'mercury' : 3.3010e23,
+    'mercury' : 3.3011e23,
     'venus'   : 4.8673e24,
     'mars'    : 6.4169e23,
     'jupiter' : Mjup,
